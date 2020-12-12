082e74d5e8cf56112942849df7dc86de6452f451
Krzwk <44804099+Krzwk@users.noreply.github.com>
2020-12-12 12:52:34 +0100

    Update PlayerController.cs
    GasComp fixed numero due

--- Horrobot/Assets/Scripts/PlayerController.cs
+++ Horrobot/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
