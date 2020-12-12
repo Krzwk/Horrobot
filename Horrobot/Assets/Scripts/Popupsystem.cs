7dcd31204346ae2a56a6eb30085a42302b39101c
Ming-Xin-Bui <73850895+Ming-Xin-Bui@users.noreply.github.com>
2020-12-12 16:20:20 +0100

    AudioSource
    Script uncompeleted

---
+++ Horrobot/Assets/Scripts/Popupsystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popupfunction : MonoBehaviour
+{
+    public gameObject popUpBox;
+    public Animator animator;
+    
+    public void PopUp(){
+        popUpBox.SetActive(true);
+        animaotr
+    }
+}
