cc6afaf2a935577cc96a6390e28e1535aad3a1f5
Krzwk <44804099+Krzwk@users.noreply.github.com>
2020-12-12 12:48:45 +0100

    Update PlayerController.cs
    GasComposition added in PlayerControlleer

--- Horrobot/Assets/Scripts/PlayerController.cs
+++ Horrobot/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,13 @@ public class PlayerController : MonoBehaviour
     public float horizontalMove = 0f;
     public float verticalMove = 0f;
 
+    private static float N2 = 78.08f;
+    private static float O2 = 20.94f;
+    private static float Ar = 0.93f;
+    
+    private static float CO2 = 0.06f;
+    private static float Kr = 3.30f;
+
     // Update is called once per frame
     void Update()
     {
@@ -27,5 +34,13 @@ public class PlayerController : MonoBehaviour
     {
         myRigid.MovePosition(myRigid.position + move * moveSpeed * Time.fixedDeltaTime);
         //controller.Move(new Vector3(horizontalMove*10*Time.deltaTime,verticalMove*10*Time.deltaTime,0));
+    }
+    public static void updateGasComp (){
+        N2 = UnityEngine.Random.Range(78.00f, 78.30f);
+        O2 = UnityEngine.Random.Range(20.80f, 20.98f);
+        Ar = UnityEngine.Random.Range(0.90f, 0.94f);
+        CO2 = UnityEngine.Random.Range(0.06f, 0.07f);
+        gasComposition.text = "N2: " + N2.ToString() + "\nO2: " + 02.ToString() + "\nAr: " + Ar.ToString()+ "\nCO2: " + CO2.ToString()+ "\nKr: " + Kr.ToString();
+
     }
 }
