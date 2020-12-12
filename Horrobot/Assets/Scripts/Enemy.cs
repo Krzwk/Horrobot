d44f10d0c98e04ab9fbd92580b148524cfc3082d
Krzwk <44804099+Krzwk@users.noreply.github.com>
2020-12-12 05:02:15 +0100

    Last version of failed repo

---
+++ Horrobot/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public float moveSpeed = 1f;
+    private Rigidbody2D rigidBody;
+    private Vector2 movement;
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rigidBody.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rigidBody.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    private void FixedUpdate()
+    {
+       moveCharacter(movement);
+    }
+}
---
+++ Horrobot/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public CharacterController controller;
+    public float moveSpeed;
+    public Rigidbody2D myRigid;
+
+    public Vector2 move;
+    public float horizontalMove = 0f;
+    public float verticalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+         move.x = Input.GetAxisRaw("Horizontal");
+         move.y = Input.GetAxisRaw("Vertical");
+         
+    }
+
+    private void FixedUpdate()
+    {
+        myRigid.MovePosition(myRigid.position + move * moveSpeed * Time.fixedDeltaTime);
+        //controller.Move(new Vector3(horizontalMove*10*Time.deltaTime,verticalMove*10*Time.deltaTime,0));
+    }
+}
