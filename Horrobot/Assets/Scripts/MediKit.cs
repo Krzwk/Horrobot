30e21972d574ec4e20452360d325db4bc6499c81
Nico <nicolas.t.ulrixh@gmail.com>
2020-12-13 12:38:24 +0100

    Medikit

---
+++ Horrobot/Assets/Scripts/MediKit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediKit : MonoBehaviour
+{ // Start is called before the first frame update
+    public PlayerController playercontr;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("test");
+            playercontr.GotRepaired();
+        }
+    }
+}
