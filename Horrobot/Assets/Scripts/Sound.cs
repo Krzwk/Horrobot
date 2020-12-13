905567cfde00190e3534e5e97add769213248d26
Ming-Xin-Bui <73850895+Ming-Xin-Bui@users.noreply.github.com>
2020-12-13 14:07:16 +0100

    Sound

---
+++ Horrobot/Assets/Scripts/Sound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    AudioSource SGJ_M1V3_Default;
+
+    public AudioClip otherClip;
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        SGJ_M1V3_Default = GetComponent<AudioSource>();
+        SGJ_M1V3_Default.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SGJ_M1V3_Default.isPlaying)
+        {
+            SGJ_M1V3_Default.clip = otherClip;
+            SGJ_M1V3_Default.Play();
+            SGJ_M1V3_Default.loop = true;
+  
+        }
+
+    }
+}
